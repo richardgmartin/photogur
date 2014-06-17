fd723e6e8379722f5a5f47297ca039665612104c
Richard Martin <richardgeorgemartin@gmail.com>
2014-06-17 17:49:08 -0400

    refactoring to save new pictures in database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,75 +1,27 @@
 class PicturesController < ApplicationController
 	def index
 		@pictures = Picture.all
-		# @pictures = [
-		# 	{
-		# 		:title  => "The old church on the coast of the White sea",
-		# 		:artist => "Sergey Ershov",
-		# 		:url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-		# 	},
-		# 	{
-		# 		:title  => "Sea Power",
-		# 		:artist => "Steve Scullion",
-		# 		:url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-		# 	},
-		# 	{
-		# 		:title  => "Into the Poppies",
-		# 		:artist => "John Wilhelm",
-		# 		:url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-		# 	}
-		# ]
 	end
 
 	def show
-		# @pictures = [
-		# 	{
-		# 		:title  => "The old church on the coast of the White sea",
-		# 		:artist => "Sergey Ershov",
-		# 		:url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-		# 	},
-		# 	{
-		# 		:title  => "Sea Power",
-		# 		:artist => "Steve Scullion",
-		# 		:url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-		# 	},
-		# 	{
-		# 		:title  => "Into the Poppies",
-		# 		:artist => "John Wilhelm",
-		# 		:url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-		# 	}
-		# ]
 		@picture = Picture.find(params[:id])
 	end
 
 	def new
+		@picture = Picture.new
 	end
 
 	def create
-		render :text => "Saving a pictue. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-
+		@picture = Picture.new(picture_params)
+		if @picture.save
+			redirect_to pictures_url
+		else
+			render :new
+		end
 	end
 
-	# def picture0
-	# 	@picture = {
-	# 		:title  => "The old church on the coast of the White sea",
-	# 		:artist => "Sergey Ershov",
-	# 		:url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-	# 	}
-	# end
-
-	# def picture1
-	# 	@picture = {
-	# 		:title  => "Sea Power",
-	# 		:artist => "Steve Scullion",
-	# 		:url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-	# 	}
-	# end
-
-	# def picture2
-	# 	@picture = {
-	# 		:title  => "Into the Poppies",
-	# 		:artist => "John Wilhelm",
-	# 		:url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-	# 	}
-	# end
+	private
+	def picture_params
+		params.require(:picture).permit(:artist, :title, :url)
+	end
 end
